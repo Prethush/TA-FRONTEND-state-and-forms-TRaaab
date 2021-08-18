ab921158d867457779987fb0a8637d0d185ec939
Prethush <ktmirash49@.gmail.com>
2021-08-18 10:44:13 +0530

    billing address

---
+++ block-BRaabs/my-app/src/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      flex: {
+        50: "0 1 47.5%"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
