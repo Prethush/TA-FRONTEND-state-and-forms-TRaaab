0632c74c880c29965fc3dc4fc9a9868e45d3c2a8
Prethush <ktmirash49@.gmail.com>
2021-08-21 14:40:21 +0530

    refactor

---
+++ block-BRaabw/code/src/components/OrderBy.js
@@ -0,0 +1,21 @@
+function OrderBy(props) {
+   
+    let allProducts = props.allProducts;
+    console.log(props);
+    return (
+        <div className="flex justify-between items-center">
+                    <h3 className="text-sm">{allProducts.length} Product(s) found.</h3>
+                    <fieldset>
+                        <label className="text-lg" htmlFor="sort">Order by</label>
+                        <select id="sort" className="p-1 ml-2 bg-white border-2 border-gray-200" name="filterPrice" value={props.filterPrice}  onChange={props.filterByPrice}>
+                            <option value="select" defaultValue>Select</option>
+                            <option value="high">Highest to Lowest</option>
+                            <option value="low">Lowest to High</option>
+                        </select>
+                        
+                    </fieldset>
+                </div>
+    )
+}
+
+export default OrderBy;
